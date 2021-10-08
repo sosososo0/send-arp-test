eee4e02b8d8889d620b8a6e4afe06c1bf0d18da8
gilgil <gilgil1973@gmail.com>
2021-10-08 07:46:26 +0000

    Update mac.cpp

--- src/mac.cpp
+++ src/mac.cpp
@@ -15,7 +15,7 @@ Mac::Mac(const std::string& r) {
 
 Mac::operator std::string() const {
 	char buf[20]; // enough size
-	sprintf(buf, "%02x:%02X:%02X:%02X:%02X:%02X", mac_[0], mac_[1], mac_[2], mac_[3], mac_[4], mac_[5]);
+	sprintf(buf, "%02X:%02X:%02X:%02X:%02X:%02X", mac_[0], mac_[1], mac_[2], mac_[3], mac_[4], mac_[5]);
 	return std::string(buf);
 }
 
